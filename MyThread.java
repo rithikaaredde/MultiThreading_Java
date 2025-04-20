68ffe4679c391c137fcf2341ef98b8ad05f0dbf9
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-04-20 15:41:55 +0530

    Updated MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -1,1 +1,22 @@
+public class MyThread extends Thread {
+    private String threadName;
 
+    public MyThread(String name) {
+        this.threadName = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(threadName + " started running.");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(threadName + ": " + i);
+            try {
+                //sleep for 5ms
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(threadName + " was interrupted.");
+            }
+        }
+        System.out.println(threadName + " finished.");
+    }
+}
